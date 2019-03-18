7272cb60511aed24019de07cb08e902ac9bcad8f
cxcxy <897939354@qq.com>
2019-03-18 16:16:52 +0800

    no message

---
+++ StringExt.swift
@@ -0,0 +1,13 @@
+//
+//  StringExt.swift
+//  Pods-XBFLutterLibrary_Example
+//
+//  Created by mac on 2019/3/18.
+//
+
+import UIKit
+
+class StringExt: NSObject {
+    var name: String?
+    var age: String?
+}
